f6100ec08757295c988038303249c92d9b4a2b03
FelipeGoncales <felipeggoncales22@gmail.com>
2025-02-25 12:50:26 -0300

    update rotas

--- user_view.py
+++ user_view.py
@@ -104,19 +104,18 @@ def update_user(id):
     cursor.close()
 
     return jsonify({
-        'message': "Usuario atualizado com sucesso!",
+        'message': "Informações atualizadas com sucesso!",
         'user': {
             'nome_completo': nome,
             'data_nascimento': data_nascimento,
             'cpf_cnpj': cpf_cnpj,
             'telefone': telefone,
-            'email': email
+            'email': email,
         }
     })
 
 
 tentativas = 0
-
 
 @app.route('/login', methods=['POST'])
 def login_user():
@@ -130,17 +129,22 @@ def login_user():
         return jsonify({"error": "Todos os campos (email, senha) são obrigatórios."}), 400
 
     cursor = con.cursor()
-    cursor.execute("SELECT id_usuario, SENHA_HASH, ativo, tipo_usuario FROM USUARIO WHERE EMAIL = ?", (email,))
+    cursor.execute("SELECT id_usuario, email, nome_completo, data_nascimento, cpf_cnpj, telefone, senha_hash, ativo, tipo_usuario FROM USUARIO WHERE EMAIL = ?", (email,))
     user_data = cursor.fetchone()
 
     if not user_data:
         cursor.close()
-        return jsonify({'message': 'Credenciais incorretas'}), 401
+        return jsonify({'message': 'Usuário não encontrado.'}), 401
 
     id_usuario = user_data[0]
-    senha_hash = user_data[1]
-    ativo = user_data[2]
-    tipo_usuario = user_data[3]
+    email = user_data[1]
+    nome_completo = user_data[2]
+    data_nascimento = user_data[3]
+    cpf_cnpj = user_data[4]
+    telefone = user_data[5]
+    senha_hash = user_data[6]
+    ativo = user_data[7]
+    tipo_usuario = user_data[8]
 
     if not ativo:
         cursor.close()
@@ -149,7 +153,18 @@ def login_user():
     if check_password_hash(senha_hash, senha):
         tentativas = 0
         cursor.close()
-        return jsonify({"message": "Login realizado com sucesso!"})
+        return jsonify({
+            "message": "Login realizado com sucesso!",
+            "dados_user": {
+                'id_usuario': id_usuario,
+                "email": email,
+                "nome_completo": nome_completo,
+                "data_nascimento": data_nascimento,
+                "cpf_cnpj": cpf_cnpj,
+                "telefone": telefone,
+                "ativo": ativo
+            }
+        })
 
     if tipo_usuario != 1:
         tentativas += 1
