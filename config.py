f08e61d03577d97babb480f4c0a9ae2dce4b59b3
FelipeGoncales <felipeggoncales22@gmail.com>
2025-05-27 14:25:23 -0300

    banco

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'NET_CARS'
 DEBUG = True
 DB_HOST = 'localhost'
-DB_NAME = r''
+DB_NAME = r'C:\Users\Aluno\Documents\GitHub\Netcars_API\upload\banco\NETCARS_TESTE.FDB'
 
 DB_USER = 'SYSDBA'
 DB_PASSWORD = 'sysdba'
